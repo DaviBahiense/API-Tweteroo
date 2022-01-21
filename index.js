3800d446c63a4486d6efeaa34cf36fd76b46a220
Davi <davibnovaes@hotmail.com>
2022-01-21 16:49:06 -0300

    projeto basico completado

--- index.js
+++ index.js
@@ -5,24 +5,35 @@ const app = express()
 app.use(cors())
 app.use(json())
 
-
 let user
-let tweet = [
+
+let tweets = [
 	{
 		username: "bobesponja",
 		avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-	  tweet: "eu amo o hub"
+	    tweet: "eu amo o hub"
 	}
 ]
 
 app.post("/sign-up", (req, res) => {
     user = req.body 
-    console.log(user)
     res.send("OK")
 });
 
 app.get("/tweets", (req, res) => {
-    res.send(tweet)
+    res.send(tweets)
+});
+
+app.post("/tweets", (req, res) => {
+    const tweet = req.body
+    tweet.avatar = user.avatar
+
+    if (tweets.length === 10) {
+        tweets.shift()
+    }
+    
+    tweets.push(tweet)
+    res.send("OK")
 });
 
 app.listen(5000)
